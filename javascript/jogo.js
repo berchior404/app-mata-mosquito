4ada530697aba78130694d72ca7a3c01047aec42
Pedro Berchior <36906171+berchior404@users.noreply.github.com>
2021-08-05 23:08:03 -0300

    posições randômicas
    adição de css e posições randômicas

--- javascript/jogo.js
+++ javascript/jogo.js
@@ -1,13 +1,28 @@
-
-var altura = 0
 var largura = 0
+var altura = 0
 
-function ajustaTamanhoPalcoJogo() {
-	altura = window.innerHeight
-	largura = window.innerWidth
+function ajustarTamanho() {
+    largura = window.innerWidth
+    altura = window.innerHeight
 
-	console.log(altura, largura)
+    console.log(altura, largura)
 }
 
-ajustaTamanhoPalcoJogo()
+ajustarTamanho()
 
+function posicaoAleatoria() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.position = 'absolute'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+
+    document.body.appendChild(mosquito)
+}
